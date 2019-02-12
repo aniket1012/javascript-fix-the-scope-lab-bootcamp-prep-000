093b4a0fbbf8630cd5b0e592a04d4b661a250a31
Aniket Patel <aniketupatel1@gmail.com>
2019-02-12 19:06:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var animal = 'dog'
+var animal = 'cat'
+
 
 function myAnimal() {
+  var animal = 'dog'
   return animal
 }
 
@@ -12,10 +14,11 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+  const two = 2
+  return (parseInt(n) + two)
 
   // Feel free to move things around!
-  const two = 2
+  
 }
 
 var funkyFunction = function() {
